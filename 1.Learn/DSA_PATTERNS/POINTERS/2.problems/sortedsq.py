bf29c23e757a78177813a34ef2ef0d7add597a51
Er-shivam11 <shivamnirmal.097@gmail.com>
2026-01-20 23:00:43 +0530

    added pointer prb

---
+++ 1.Learn/DSA_PATTERNS/POINTERS/2.problems/sortedsq.py
@@ -0,0 +1,21 @@
+def sortedSquares(nums):
+    n = len(nums)
+    result = [0] * n
+
+    left = 0
+    right = n - 1
+    pos = n - 1   # fill from back
+
+    while left <= right:
+        if abs(nums[left]) > abs(nums[right]):
+            result[pos] = nums[left] * nums[left]
+            left += 1
+        else:
+            result[pos] = nums[right] * nums[right]
+            right -= 1
+        pos -= 1
+
+    return result
+
+nums = [-4, -1, 0, 3, 10]
+print(sortedSquares(nums))
