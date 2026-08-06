edf2e6744208078eaab55b0507303c9592ef3203
Er-shivam11 <shivamnirmal.097@gmail.com>
2026-08-06 18:07:37 +0530

    CLEANED AND UPDATED FLOW FOR GOV AND TECH

--- 2.Build/1.py/practise.py
+++
@@ -1,2 +0,0 @@
-with open("rawcode.txt", "w") as file:
-    file.write("Hello Shivam")
---
+++ 2.Build/1.tech/1.py/practise.py
@@ -0,0 +1,10 @@
+# nums = [2,7,11,15]
+# target = 9
+# for i in nums:
+#     print([nums[i]])
+
+d=[3,2,2,1,4]
+b=d.sort()
+print(b)
+
+
